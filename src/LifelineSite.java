8a660fa30cf85aaa764f39a8bbadd7f10cf99221
kylet <kyletaticek@cmail.carleton.ca>
2022-11-14 16:32:02 -0500

    Added methods to LifelineSite

--- src/LifelineSite.java
+++ src/LifelineSite.java
@@ -1,9 +1,17 @@
 public class LifelineSite extends Site{
+
     private double _rate;
     private double _units;
 
+    @Override
+    protected double getTaxAmount(){
+        return 0;
+    }
 
-
+    @Override
+    protected double getBaseAmount(){
+        return 0;
+    }
     public double getBillableAmount(){
         double base = _units * _rate * 0.5;
         double tax = base * Site.TAX_RATE * 0.2;
