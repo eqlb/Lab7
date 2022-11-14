d9d93c1a2baad877b0b46df33aaf4e5298386c78
kylet <kyletaticek@cmail.carleton.ca>
2022-11-14 16:13:35 -0500

    Extracted getBaseAmount() from getBillableAmount()

--- src/ResidentialSite.java
+++ src/ResidentialSite.java
@@ -1,11 +1,16 @@
 public class ResidentialSite extends Site{
 
 
-
+    @Override
     public double getBillableAmount(double _units, double _rate){
-        double base = _units * _rate;
+        double base = getBaseAmount(_units, _rate);
         double tax = base * Site.TAX_RATE;
         return base + tax;
+    }
+
+    private static double getBaseAmount(double _units, double _rate){
+        double base = _units * _rate * 0.5;
+        return base;
     }
 
 
