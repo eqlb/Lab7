2fb2a9f9ade8fb5df42b7fbdb3f1e82b163456c9
kylet <kyletaticek@cmail.carleton.ca>
2022-11-14 16:00:49 -0500

    Began Refactoring, Added methods to Site

--- src/Site.java
+++ src/Site.java
@@ -1,6 +1,18 @@
 public class Site {
     static double TAX_RATE;
 
+
+    public double getBillableAmount(){
+        return getBaseAmount() + getTaxAmount();
+    }
+
+    public double getBaseAmount(){
+        return 0.0;
+    }
+    public double getTaxAmount(){
+        return 0.0;
+    }
+
 }
 
 
